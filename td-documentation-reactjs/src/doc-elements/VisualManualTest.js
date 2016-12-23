ad4956aa68c1fb5450af8a2cb4371afb9e1d58ec
MykolaGolubyev <golubev.nikolay@gmail.com>
2016-12-22 21:26:51 -0500

    lambda instead of a class

--- td-documentation-reactjs/src/doc-elements/VisualManualTest.js
+++ td-documentation-reactjs/src/doc-elements/VisualManualTest.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import DocElement from './DocElement'
 import elementsLibrary from './DefaultElementsLibrary'
 import TestData from './TestData'
 
-class VisualManualTest extends Component {
-  render() {
-    return (
+const VisualManualTest = () => (
       <div className="container">
         <div className="row">
           <DocElement content={TestData.simplePage} elementsLibrary={elementsLibrary}/>
         </div>
       </div>
-    );
-  }
-}
+    )
 
 export default VisualManualTest
