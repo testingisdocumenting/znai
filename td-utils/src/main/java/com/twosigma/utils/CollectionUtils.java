c83f2531b2e43c95cb0c34d85e9e92976b48a42a
MykolaGolubyev <golubev.nikolay@gmail.com>
2016-12-31 12:57:55 -0500

    collection utils, create map from varargs

---
+++ td-utils/src/main/java/com/twosigma/utils/CollectionUtils.java
@@ -0,0 +1,37 @@
+package com.twosigma.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author mykola
+ */
+public class CollectionUtils {
+    private CollectionUtils() {
+    }
+
+    /**
+     * creates map from var args key value
+     * @param k first key
+     * @param v first value
+     * @param kvs key value pairs
+     * @param <K> type of key
+     * @param <V> type of value
+     * @return map with preserved order
+     */
+    @SuppressWarnings("unchecked")
+    public static <K, V> Map<K, V> createMap(K k, V v, Object... kvs) {
+        Map<K, V> result = new LinkedHashMap<>();
+        result.put(k, v);
+
+        if (kvs.length % 2 != 0) {
+            throw new IllegalArgumentException("key value sequence must have even number of values");
+        }
+
+        for (int idx = 0; idx < kvs.length; idx += 2) {
+            result.put((K) kvs[idx], (V) kvs[idx + 1]);
+        }
+
+        return result;
+    }
+}
