60273fb4edd83604370d47878b420727aac5bd92
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-01-07 11:45:44 -0500

    regexp utils replace with callback

---
+++ td-utils/src/main/java/com/twosigma/utils/RegexpUtils.java
@@ -0,0 +1,26 @@
+package com.twosigma.utils;
+
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author mykola
+ */
+public class RegexpUtils {
+    private RegexpUtils() {
+    }
+
+    public static String replaceAll(String source, Pattern regexp, Function<Matcher, String> replacement) {
+        Matcher matcher = regexp.matcher(source);
+        StringBuffer result = new StringBuffer();
+
+        while (matcher.find()) {
+            matcher.appendReplacement(result, replacement.apply(matcher));
+        }
+
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+}
