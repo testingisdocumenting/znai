da2228b68494d801d6d028082a6c4331803887c0
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-01-09 16:54:25 -0500

    nested content null check

--- td-documentation-reactjs/src/doc-elements/DocElement.js
+++ td-documentation-reactjs/src/doc-elements/DocElement.js
@@ -4,7 +4,7 @@ import * as React from 'react'
  * uses given set of components to render DocElements like links, paragraphs, code blocks, etc
  */
 const DocElement = ({content, elementsLibrary}) => {
-    return (<span>{content.map((item, idx) => {
+    return (<span>{!content ? null : content.map((item, idx) => {
         const ElementToUse = elementsLibrary[item.type]
         if (!ElementToUse) {
             console.warn("can't find component to display", item)
