a8e444357524de5ffcfb186a20c73456c1d2f6db
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-01-09 16:32:06 -0500

    temp app to run data generation for diagram flow UI

---
+++ td-documentation/src/test/groovy/com/twosigma/documentation/diagrams/markdown/SlidePocTodoRemove.groovy
@@ -0,0 +1,34 @@
+package com.twosigma.documentation.diagrams.markdown
+
+import com.twosigma.diagrams.graphviz.GraphvizEngine
+import com.twosigma.diagrams.graphviz.InteractiveCmdGraphviz
+import com.twosigma.diagrams.graphviz.meta.GraphvizShapeConfig
+import com.twosigma.utils.FileUtils
+import com.twosigma.utils.JsonUtils
+import com.twosigma.utils.ResourceUtils
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * @author mykola
+ */
+class SlidePocTodoRemove {
+    static void main(String[] args) {
+        def graphviz = ResourceUtils.textContent("test.gv")
+        def markdown = ResourceUtils.textContent("test-flow.md")
+
+        def shapeConfig = new GraphvizShapeConfig(ResourceUtils.textContent("graphviz-meta-conf.json"))
+        def runtime = new InteractiveCmdGraphviz()
+        def engine = new GraphvizEngine(runtime, shapeConfig)
+
+
+        Map<String, Object> diagramPresentation = new LinkedHashMap<>()
+
+        diagramPresentation.put("slides", MarkdownDiagramSlides.createSlides(markdown).toListOfMaps())
+        diagramPresentation.put("diagram", engine.diagramFromGv(graphviz))
+
+        String testData = "const data = " + JsonUtils.serializePrettyPrint(diagramPresentation) + "\n\nexport default data"
+        Files.write(Paths.get("/Users/mykola/work/testing-documenting/td-documentation-reactjs/src/doc-elements/DiagramSlidesTestData.js"), testData.getBytes())
+    }
+}
---
+++ td-documentation/src/test/resources/test-flow.md
@@ -0,0 +1,52 @@
+# human
+
+human **responsibilities** are
+
+1. one
+2. two
+3. three
+
+# world
+
+*soo many* issue are right now with the world
+
+# server
+
+Servers exist to make life easier
+
+# test
+# server->test
+
+First we need to establish a **connection** between and
+
+# test->human
+
+To test humanity we will use this snippet
+
+    class code {
+       block;
+    }
+   
+# another
+# server->another
+
+It is important to _listen_
+to bullet 
+
+* points
+* taken
+
+# next
+# human->next
+
+Next iteration will be to see what learning & development will do
+
+> Important 
+> Quote
+
+# another->world
+# human->world
+
+Either humanity will learn
+********
+Or another species 
