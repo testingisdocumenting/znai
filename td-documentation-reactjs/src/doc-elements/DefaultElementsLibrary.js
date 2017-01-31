b267ec644198a717980aacc336b95b6b50754d16
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-01-30 19:28:08 -0500

    remove id gen as it is not used for diff highlight anymore

--- td-documentation-reactjs/src/doc-elements/DefaultElementsLibrary.js
+++ td-documentation-reactjs/src/doc-elements/DefaultElementsLibrary.js
@@ -4,58 +4,47 @@ import Snippet from './Snippet'
 import GraphVizSvg from './graphviz/GraphVizSvg'
 import GraphVizFlow from './graphviz/GraphVizFlow'
 
-class Id {
-    constructor() {
-        this._id = 0
-    }
-
-    next() {
-        this._id++
-        return this._id
-    }
-}
-
 const library = {}
 
-const BoundDocElement = ({content, idGen}) => <DocElement content={content} elementsLibrary={library} idGen={idGen}/>
+const BoundDocElement = ({content}) => <DocElement content={content} elementsLibrary={library}/>
 
 library.DocElement = BoundDocElement
-library.Emphasis = ({content, idGen}) => (<span className="emphasis"><BoundDocElement content={content} idGen={idGen}/></span>)
-library.StrongEmphasis = ({content, idGen}) => (<span className="strong-emphasis"><BoundDocElement content={content} idGen={idGen}/></span>)
-library.Link = ({anchor, label, idGen}) => (<a href={anchor} data-id={idGen.next()}>{label}</a>)
-library.Paragraph = ({content, idGen}) => <div className="paragraph content-block" data-id={idGen.next()}><BoundDocElement content={content} idGen={idGen}/></div>
-library.BlockQuote = ({content, idGen}) => <blockquote className="content-block"><BoundDocElement content={content} idGen={idGen}/></blockquote>
-library.SimpleText = ({text, idGen}) => <span className="simple-text" data-id={idGen.next()}>{text}</span>
+library.Emphasis = ({content}) => (<span className="emphasis"><BoundDocElement content={content}/></span>)
+library.StrongEmphasis = ({content}) => (<span className="strong-emphasis"><BoundDocElement content={content}/></span>)
+library.Link = ({anchor, label}) => (<a href={anchor}>{label}</a>)
+library.Paragraph = ({content}) => <div className="paragraph content-block"><BoundDocElement content={content}/></div>
+library.BlockQuote = ({content}) => <blockquote className="content-block"><BoundDocElement content={content}/></blockquote>
+library.SimpleText = ({text}) => <span className="simple-text">{text}</span>
 library.SoftLineBreak = () => <span> </span>
 library.HardLineBreak = () => <br />
 library.ThematicBreak = () => <hr />
 
 library.Snippet = Snippet
 
-library.BulletList = ({tight, bulletMarker, content, idGen}) => {
+library.BulletList = ({tight, bulletMarker, content}) => {
     const className = "content-block" + (tight ? " tight" : "")
-    return (<ul className={className}><BoundDocElement content={content} idGen={idGen}/></ul>)
+    return (<ul className={className}><BoundDocElement content={content}/></ul>)
 }
 
-library.OrderedList = ({delimiter, startNumber, content, idGen}) => <ol className="content-block" start={startNumber}><BoundDocElement content={content} idGen={idGen}/></ol>
+library.OrderedList = ({delimiter, startNumber, content}) => <ol className="content-block" start={startNumber}><BoundDocElement content={content}/></ol>
 
-library.ListItem = ({content, idGen}) => <li><BoundDocElement content={content} idGen={idGen}/></li>
+library.ListItem = ({content}) => <li><BoundDocElement content={content}/></li>
 
-library.Section = ({title, content, idGen}) => (
+library.Section = ({title, content}) => (
         <div className="section" key={title}>
             <div className="content-block">
-                <div className="section-title" data-id={idGen.next()}>{title}</div>
+                <div className="section-title">{title}</div>
             </div>
-            <BoundDocElement content={content} idGen={idGen}/>
+            <BoundDocElement content={content}/>
         </div>)
 
-library.CustomComponent = ({componentName, componentProps, idGen}) => {
+library.CustomComponent = ({componentName, componentProps}) => {
     const Component = library[componentName]
     if (!Component) {
         console.warn("can't find a custom component: ", componentName)
         return null
     } else {
-        return <Component {...componentProps} idGen={idGen}/>
+        return <Component {...componentProps}/>
     }
 }
 
@@ -63,7 +52,7 @@ library.GraphVizSvg = GraphVizSvg
 library.GraphVizFlow = GraphVizFlow
 
 library.Page = ({title, content}) => (<div className="page-content">
-    <BoundDocElement key={title} content={content} idGen={new Id()}/>
+    <BoundDocElement key={title} content={content}/>
 </div>)
 
 export default library
--- td-documentation-reactjs/src/doc-elements/DocElement.js
+++ td-documentation-reactjs/src/doc-elements/DocElement.js
@@ -4,17 +4,16 @@ import * as React from 'react'
  * uses given set of components to render DocElements like links, paragraphs, code blocks, etc
  *
  * @param content content to render
- * @param idGen id generator for content
  * @param elementsLibrary library of elements to use to render
  */
-const DocElement = ({content, idGen, elementsLibrary}) => {
+const DocElement = ({content, elementsLibrary}) => {
     return (<span>{!content ? null : content.map((item, idx) => {
         const ElementToUse = elementsLibrary[item.type]
         if (!ElementToUse) {
             console.warn("can't find component to display", item)
             return null
         } else {
-            return <ElementToUse key={idx} idGen={idGen} {...item} />
+            return <ElementToUse key={idx} {...item} />
         }
     })}</span>)
 }
