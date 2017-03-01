53a9cb60bc80a1a724ac6b2e63a25a052f5930f0
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-03-01 06:12:19 -0500

    update documentation local ui dev data

--- td-documentation-reactjs/public/toc.js
+++ td-documentation-reactjs/public/toc.js
@@ -1,234 +1,250 @@
 toc = [
-    {
+  {
+    "sectionTitle": "",
+    "dirName": "",
+    "items": [
+      {
         "sectionTitle": "",
+        "pageTitle": "Index",
+        "fileName": "index",
         "dirName": "",
-        "items": [
-            {
-                "sectionTitle": "",
-                "pageTitle": "Index",
-                "fileName": "index",
-                "dirName": "",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "what",
-                        "id": "what"
-                    },
-                    {
-                        "title": "why",
-                        "id": "why"
-                    },
-                    {
-                        "title": "how",
-                        "id": "how"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "what",
+            "id": "what"
+          },
+          {
+            "title": "why",
+            "id": "why"
+          },
+          {
+            "title": "how",
+            "id": "how"
+          }
         ]
-    },
-    {
+      }
+    ]
+  },
+  {
+    "sectionTitle": "Rationale",
+    "dirName": "rationale",
+    "items": [
+      {
         "sectionTitle": "Rationale",
+        "pageTitle": "Engineers",
+        "fileName": "engineers",
         "dirName": "rationale",
-        "items": [
-            {
-                "sectionTitle": "Rationale",
-                "pageTitle": "Engineers",
-                "fileName": "engineers",
-                "dirName": "rationale",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Focus On Data",
-                        "id": "focus-on-data"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "Rationale",
-                "pageTitle": "Setup",
-                "fileName": "setup",
-                "dirName": "rationale",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Kerberos",
-                        "id": "kerberos"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "Rationale",
-                "pageTitle": "First Code Base",
-                "fileName": "first-code-base",
-                "dirName": "rationale",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Simple Java",
-                        "id": "simple-java"
-                    },
-                    {
-                        "title": "Web",
-                        "id": "web"
-                    },
-                    {
-                        "title": "Python",
-                        "id": "python"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "Focus On Data",
+            "id": "focus-on-data"
+          }
         ]
-    },
-    {
+      },
+      {
+        "sectionTitle": "Rationale",
+        "pageTitle": "Setup",
+        "fileName": "setup",
+        "dirName": "rationale",
+        "pageSectionIdTitles": [
+          {
+            "title": "Kerberos",
+            "id": "kerberos"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "Rationale",
+        "pageTitle": "First Code Base",
+        "fileName": "first-code-base",
+        "dirName": "rationale",
+        "pageSectionIdTitles": [
+          {
+            "title": "Simple Java",
+            "id": "simple-java"
+          },
+          {
+            "title": "Web",
+            "id": "web"
+          },
+          {
+            "title": "Python",
+            "id": "python"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "Features",
+    "dirName": "features",
+    "items": [
+      {
         "sectionTitle": "Features",
+        "pageTitle": "Code Snippets",
+        "fileName": "code-snippets",
         "dirName": "features",
-        "items": [
-            {
-                "sectionTitle": "Features",
-                "pageTitle": "Code Snippets",
-                "fileName": "code-snippets",
-                "dirName": "features",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Auto Highlight",
-                        "id": "auto-highlight"
-                    },
-                    {
-                        "title": "Specifying Language",
-                        "id": "specifying-language"
-                    },
-                    {
-                        "title": "Wide Code",
-                        "id": "wide-code"
-                    },
-                    {
-                        "title": "From File",
-                        "id": "from-file"
-                    },
-                    {
-                        "title": "Inlined",
-                        "id": "inlined"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "Features",
-                "pageTitle": "Tables",
-                "fileName": "tables",
-                "dirName": "features",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "CSV",
-                        "id": "csv"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "Features",
-                "pageTitle": "Proxy",
-                "fileName": "proxy",
-                "dirName": "features",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Collecting Data",
-                        "id": "collecting-data"
-                    },
-                    {
-                        "title": "Log Stash",
-                        "id": "log-stash"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "Simple Snippet",
+            "id": "simple-snippet"
+          },
+          {
+            "title": "Specifying Language",
+            "id": "specifying-language"
+          },
+          {
+            "title": "Wide Code",
+            "id": "wide-code"
+          },
+          {
+            "title": "Inlined",
+            "id": "inlined"
+          }
         ]
-    },
-    {
+      },
+      {
+        "sectionTitle": "Features",
+        "pageTitle": "External Code Snippets",
+        "fileName": "external-code-snippets",
+        "dirName": "features",
+        "pageSectionIdTitles": [
+          {
+            "title": "Existing File",
+            "id": "existing-file"
+          },
+          {
+            "title": "Inline Comments",
+            "id": "inline-comments"
+          },
+          {
+            "title": "Callout Comments",
+            "id": "callout-comments"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "Features",
+        "pageTitle": "Tables",
+        "fileName": "tables",
+        "dirName": "features",
+        "pageSectionIdTitles": [
+          {
+            "title": "CSV",
+            "id": "csv"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "Features",
+        "pageTitle": "Proxy",
+        "fileName": "proxy",
+        "dirName": "features",
+        "pageSectionIdTitles": [
+          {
+            "title": "Collecting Data",
+            "id": "collecting-data"
+          },
+          {
+            "title": "Log Stash",
+            "id": "log-stash"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "Cook Book",
+    "dirName": "cook-book",
+    "items": [
+      {
         "sectionTitle": "Cook Book",
+        "pageTitle": "Merging Time Series",
+        "fileName": "merging-time-series",
         "dirName": "cook-book",
-        "items": [
-            {
-                "sectionTitle": "Cook Book",
-                "pageTitle": "Merging Time Series",
-                "fileName": "merging-time-series",
-                "dirName": "cook-book",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Time Series",
-                        "id": "time-series"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "Time Series",
+            "id": "time-series"
+          }
         ]
-    },
-    {
+      }
+    ]
+  },
+  {
+    "sectionTitle": "External Code Bases",
+    "dirName": "external-code-bases",
+    "items": [
+      {
         "sectionTitle": "External Code Bases",
+        "pageTitle": "External Code To Vats",
+        "fileName": "external-code-to-vats",
         "dirName": "external-code-bases",
-        "items": [
-            {
-                "sectionTitle": "External Code Bases",
-                "pageTitle": "External Code To Vats",
-                "fileName": "external-code-to-vats",
-                "dirName": "external-code-bases",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "test",
-                        "id": "test"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "External Code Bases",
-                "pageTitle": "Maven Import",
-                "fileName": "maven-import",
-                "dirName": "external-code-bases",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "description",
-                        "id": "description"
-                    },
-                    {
-                        "title": "codebase naming",
-                        "id": "codebase-naming"
-                    },
-                    {
-                        "title": "Usage Scenario",
-                        "id": "usage-scenario"
-                    }
-                ]
-            },
-            {
-                "sectionTitle": "External Code Bases",
-                "pageTitle": "License Restrictions",
-                "fileName": "license-restrictions",
-                "dirName": "external-code-bases",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "Acceptable for Two Sigma",
-                        "id": "acceptable-for-two-sigma"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "test",
+            "id": "test"
+          }
         ]
-    },
-    {
+      },
+      {
+        "sectionTitle": "External Code Bases",
+        "pageTitle": "Maven Import",
+        "fileName": "maven-import",
+        "dirName": "external-code-bases",
+        "pageSectionIdTitles": [
+          {
+            "title": "description",
+            "id": "description"
+          },
+          {
+            "title": "codebase naming",
+            "id": "codebase-naming"
+          },
+          {
+            "title": "Usage Scenario",
+            "id": "usage-scenario"
+          }
+        ]
+      },
+      {
+        "sectionTitle": "External Code Bases",
+        "pageTitle": "License Restrictions",
+        "fileName": "license-restrictions",
+        "dirName": "external-code-bases",
+        "pageSectionIdTitles": [
+          {
+            "title": "Acceptable for Two Sigma",
+            "id": "acceptable-for-two-sigma"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "sectionTitle": "Demo Run Book",
+    "dirName": "demo-run-book",
+    "items": [
+      {
         "sectionTitle": "Demo Run Book",
+        "pageTitle": "Release Notes",
+        "fileName": "release-notes",
         "dirName": "demo-run-book",
-        "items": [
-            {
-                "sectionTitle": "Demo Run Book",
-                "pageTitle": "Release Notes",
-                "fileName": "release-notes",
-                "dirName": "demo-run-book",
-                "pageSectionIdTitles": [
-                    {
-                        "title": "0.2",
-                        "id": "0.2"
-                    },
-                    {
-                        "title": "0.1.1",
-                        "id": "0.1.1"
-                    },
-                    {
-                        "title": "0.1",
-                        "id": "0.1"
-                    }
-                ]
-            }
+        "pageSectionIdTitles": [
+          {
+            "title": "0.2",
+            "id": "0.2"
+          },
+          {
+            "title": "0.1.1",
+            "id": "0.1.1"
+          },
+          {
+            "title": "0.1",
+            "id": "0.1"
+          }
         ]
-    }
+      }
+    ]
+  }
 ]
