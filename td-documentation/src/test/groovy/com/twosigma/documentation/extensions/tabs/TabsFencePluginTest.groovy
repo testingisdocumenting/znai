76539bc94696f51d0b20ec49d33679da0c6447d0
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-05-05 10:04:33 -0400

    fix test

--- td-documentation/src/test/groovy/com/twosigma/documentation/extensions/tabs/TabsFencePluginTest.groovy
+++ td-documentation/src/test/groovy/com/twosigma/documentation/extensions/tabs/TabsFencePluginTest.groovy
@@ -1,6 +1,6 @@
 package com.twosigma.documentation.extensions.tabs
 
-import com.twosigma.documentation.extensions.PluginResult
+import com.twosigma.documentation.extensions.PluginParams
 import com.twosigma.documentation.parser.TestComponentsRegistry
 import org.junit.Test
 
@@ -13,7 +13,8 @@ class TabsFencePluginTest {
     @Test
     void "include plugin per tab"() {
         def plugin = new TabsFencePlugin()
-        def result = plugin.process(new TestComponentsRegistry(), Paths.get("test.md"), "java:include-dummy: ff1 {p1: 'v1'}\n" +
+        def result = plugin.process(new TestComponentsRegistry(), Paths.get("test.md"), new PluginParams("tabs", ""),
+                "java:include-dummy: ff1 {p1: 'v1'}\n" +
                 "groovy:include-dummy:xz ff2 {p2: 'v2'}")
 
         result.docElements.collect { it.toMap() }.should == [[tabsContent: [[name: 'java', content: [[ff: 'ff1', opts: [p1: 'v1'], type: 'IncludeDummy']]],
