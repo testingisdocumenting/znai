a99be2279ffed54ef566527900b1fcffeed74215
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-05-27 20:56:45 -0400

    add icons inlined code plugin

---
+++ td-documentation-cli/documentation/features/icons.md
@@ -0,0 +1,9 @@
+# Glyphicons
+
+Icons can be included using *inlined code plugin*.
+
+    `icon:cloud`
+    
+Result will inlined into the current paragraph `icon:cloud`
+
+List of icons is available here: [http://getbootstrap.com/components/#glyphicons](http://getbootstrap.com/components/#glyphicons)
---
+++ td-documentation/src/main/java/com/twosigma/documentation/extensions/icons/IconInlinedCodePlugin.java
@@ -0,0 +1,24 @@
+package com.twosigma.documentation.extensions.icons;
+
+import com.twosigma.documentation.core.ComponentsRegistry;
+import com.twosigma.documentation.extensions.PluginParams;
+import com.twosigma.documentation.extensions.PluginResult;
+import com.twosigma.documentation.extensions.inlinedcode.InlinedCodePlugin;
+import com.twosigma.documentation.parser.docelement.DocElement;
+
+import java.nio.file.Path;
+
+/**
+ * @author mykola
+ */
+public class IconInlinedCodePlugin implements InlinedCodePlugin {
+    @Override
+    public String id() {
+        return "icon";
+    }
+
+    @Override
+    public PluginResult process(ComponentsRegistry componentsRegistry, Path markupPath, PluginParams pluginParams) {
+        return PluginResult.docElement(new DocElement("Icon", "id", pluginParams.getFreeParam()));
+    }
+}
