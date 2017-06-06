fbcaa6f05a5c30e9a42667f1855e2186e1881c4d
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-06-05 21:44:54 -0400

    tweaking paragraph attention signs margin

--- td-documentation-reactjs/src/doc-elements/default-elements/ParagraphDemo.js
+++ td-documentation-reactjs/src/doc-elements/default-elements/ParagraphDemo.js
@@ -35,8 +35,81 @@ function createMessage(suffix) {
 
 }
 
+const code = {
+    "lang": "javascript",
+    "maxLineLength": 52,
+    "tokens": [
+        {
+            "type": "keyword",
+            "content": "class"
+        },
+        " ",
+        {
+            "type": "class-name",
+            "content": [
+                "JsClass"
+            ]
+        },
+        " ",
+        {
+            "type": "punctuation",
+            "content": "{"
+        },
+        "\n    ",
+        {
+            "type": "function",
+            "content": "constructor"
+        },
+        {
+            "type": "punctuation",
+            "content": "("
+        },
+        {
+            "type": "punctuation",
+            "content": ")"
+        },
+        " ",
+        {
+            "type": "punctuation",
+            "content": "{"
+        },
+        " ",
+        {
+            "type": "comment",
+            "content": "// new syntax for constructor"
+        },
+        "\n    ",
+        {
+            "type": "punctuation",
+            "content": "}"
+        },
+        "\n",
+        {
+            "type": "punctuation",
+            "content": "}"
+        },
+        "\n\n",
+        {
+            "type": "keyword",
+            "content": "export"
+        },
+        " ",
+        {
+            "type": "keyword",
+            "content": "default"
+        },
+        " JsClass ",
+        {
+            "type": "comment",
+            "content": "// new syntax for ES6 modules"
+        }
+    ],
+    "type": "Snippet"
+}
+
+
 const ParagraphDemo = () => {
-    const content = [noteParagraph, warningParagraph, avoidParagraph, questionParagraph]
+    const content = [createMessage(""), noteParagraph, code, warningParagraph, avoidParagraph, questionParagraph]
     return <elementsLibrary.DocElement content={content} elementsLibrary={elementsLibrary}/>
 }
 
