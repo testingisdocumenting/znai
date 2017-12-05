8f5ae9581e3f1f734a8c3acfe7325e38aa6d6f0c
MykolaGolubyev <golubev.nikolay@gmail.com>
2017-12-05 12:06:28 -0500

    fix test

--- td-documentation-core/src/test/groovy/com/twosigma/documentation/search/SiteSearchEntriesTest.groovy
+++ td-documentation-core/src/test/groovy/com/twosigma/documentation/search/SiteSearchEntriesTest.groovy
@@ -10,8 +10,8 @@ class SiteSearchEntriesTest {
     void "should generate XML document"() {
         def entries = new SiteSearchEntries()
         entries.addAll([
-                new SiteSearchEntry('full title 1', new SearchText('text 1', SearchScore.HIGH)),
-                new SiteSearchEntry('full title 2', new SearchText('text 2', SearchScore.STANDARD))])
+                new SiteSearchEntry('/doc-id/title1','full title 1', new SearchText('text 1', SearchScore.HIGH)),
+                new SiteSearchEntry('/doc-id/title2','full title 2', new SearchText('text 2', SearchScore.STANDARD))])
 
         entries.toXml().should == '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
                 '<mdoc>\n' +
@@ -21,6 +21,7 @@ class SiteSearchEntriesTest {
                 '            <score>HIGH</score>\n' +
                 '            <text>text 1</text>\n' +
                 '        </text>\n' +
+                '        <url>/doc-id/title1</url>\n' +
                 '    </entry>\n' +
                 '    <entry>\n' +
                 '        <fullTitle>full title 2</fullTitle>\n' +
@@ -28,6 +29,7 @@ class SiteSearchEntriesTest {
                 '            <score>STANDARD</score>\n' +
                 '            <text>text 2</text>\n' +
                 '        </text>\n' +
+                '        <url>/doc-id/title2</url>\n' +
                 '    </entry>\n' +
                 '</mdoc>\n'
     }
