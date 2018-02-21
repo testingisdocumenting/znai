537cc75928d775acf20d3ef7ef36765f869d1996
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-02-20 21:17:44 -0500

    fix test

--- td-documentation-sphinx/src/test/groovy/com/twosigma/documentation/parser/sphinx/python/PythonClassTest.groovy
+++ td-documentation-sphinx/src/test/groovy/com/twosigma/documentation/parser/sphinx/python/PythonClassTest.groovy
@@ -13,11 +13,11 @@ class PythonClassTest {
     void "should convert to map"() {
         def pythonClass = new PythonClass('ClassName', 'class description')
         def method1 = new PythonFunction('methodOne', 'method one description')
-        method1.addParam(new PythonFunctionParam('param1', 'param1 description'))
-        method1.addParam(new PythonFunctionParam('param2', 'param2 description'))
+        method1.addParam(new PythonFunctionParam('param1', 'str', 'param1 description'))
+        method1.addParam(new PythonFunctionParam('param2', 'str', 'param2 description'))
 
         def method2 = new PythonFunction('methodTwo', 'method two description')
-        method2.addParam(new PythonFunctionParam('param2', 'param2 description'))
+        method2.addParam(new PythonFunctionParam('param2',  'str', 'param2 description'))
 
         pythonClass.addMethod(method1)
         pythonClass.addMethod(method2)
@@ -25,9 +25,11 @@ class PythonClassTest {
         def map = pythonClass.toMap()
         actual(map).should(equal([name: 'ClassName', description: 'class description',
                                   methods:[[name: 'methodOne', description: 'method one description',
-                                                            params:[[name: 'param1', description: 'param1 description'],
-                                                                    [name: 'param2', description: 'param2 description']]],
-                                                           [name: 'methodTwo', description: 'method two description',
-                                                             params:[[name: 'param2', description: 'param2 description']]]]]))
+                                            params:[[name: 'param1', type: 'str', description: 'param1 description'],
+                                                    [name: 'param2', type: 'str', description: 'param2 description']],
+                                            paramSignatures: []],
+                                          [name: 'methodTwo', description: 'method two description',
+                                           params:[[name: 'param2', type: 'str', description: 'param2 description']],
+                                           paramSignatures: []]]]))
     }
 }
