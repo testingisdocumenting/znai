5cb4bbb4d0e91e0fd091fad93acce683d29e440e
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-03-03 19:26:10 -0500

    api sections css tweaks. introduce css var mdoc-function-color

--- td-documentation-reactjs/src/doc-elements/page/api/ApiSection.js
+++ td-documentation-reactjs/src/doc-elements/page/api/ApiSection.js
@@ -3,18 +3,29 @@ import React from 'react'
 import './ApiSection.css'
 
 const ApiSection = ({elementsLibrary, id, title, isSelected, content}) => {
-    const titleClassName = 'title ' + (isSelected ? ' selected' : '')
+    const titleClassName = 'title-block' + (isSelected ? ' selected' : '')
 
     return (
         <div className="api-section content-block">
             <a name={id} href={'#' + id}>
-                <div className={titleClassName}>{title}</div>
+                <div className={titleClassName}>
+                    <CollapseIndicator isSelected={isSelected}/>
+                    <div className="title">{title}</div>
+                </div>
             </a>
 
 
             {isSelected && <elementsLibrary.DocElement elementsLibrary={elementsLibrary}
                                                        content={content}/>}
         </div>
+    )
+}
+
+function CollapseIndicator({isSelected}) {
+    const className = 'collapse-indicator ' + (isSelected ? 'uncollapsed' : 'collapsed')
+
+    return (
+        <div className={className}/>
     )
 }
 
