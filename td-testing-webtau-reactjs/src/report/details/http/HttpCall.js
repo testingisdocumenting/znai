935641c6a58f7a1a64f15af115d18c88aafe25d6
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-04-22 12:37:55 -0400

    webtau report make http calls details collapsable

--- td-testing-webtau-reactjs/src/report/details/http/HttpCall.js
+++ td-testing-webtau-reactjs/src/report/details/http/HttpCall.js
@@ -3,14 +3,9 @@ import './HttpCall.css'
 
 import Payload from './Payload'
 
-const HttpCall = ({httpCall}) => {
-    return (
-        <div className="http-call">
-            <div className="method-and-url">
-                <span className="method">{httpCall.method}</span>
-                <span className="url">{httpCall.url}</span>
-            </div>
-
+const HttpCall = ({idx, httpCall, isExpanded, onCollapseToggleClick}) => {
+    const renderedDetails = isExpanded && (
+        <React.Fragment>
             <div className="request">
                 <Payload caption="Request"
                          type={httpCall.requestType}
@@ -23,6 +18,21 @@ const HttpCall = ({httpCall}) => {
                          data={httpCall.responseBody}
                          checks={httpCall.responseBodyChecks}/>
             </div>
+        </React.Fragment>
+    )
+
+    return (
+        <div className="http-call">
+            <div className="method-and-url">
+                <div className="collapse-toggle"
+                      onClick={() => onCollapseToggleClick(idx)}>
+                    {isExpanded ? '-' : '+'}
+                </div>
+                <div className="method">{httpCall.method}</div>
+                <div className="url">{httpCall.url}</div>
+            </div>
+
+            {renderedDetails}
         </div>
     )
 }
--- td-testing-webtau-reactjs/src/report/details/http/HttpCalls.js
+++ td-testing-webtau-reactjs/src/report/details/http/HttpCalls.js
@@ -3,14 +3,39 @@ import React, {Component} from 'react'
 import HttpCall from './HttpCall'
 
 class HttpCalls extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { expandedByIdx: {} }
+    }
+
     render() {
         const {test} = this.props;
 
         return (
             <div className="http">
-                {test.httpCalls.map((httpCall, idx) => <HttpCall key={idx} httpCall={httpCall}/>)}
+                {test.httpCalls.map((httpCall, idx) => <HttpCall key={idx}
+                                                                 idx={idx}
+                                                                 httpCall={httpCall}
+                                                                 isExpanded={this.isExpanded(idx)}
+                                                                 onCollapseToggleClick={this.onCollapseToggleClick}/>)}
             </div>
         )
+    }
+
+    isExpanded(idx) {
+        return this.state.expandedByIdx.hasOwnProperty(idx)
+    }
+
+    onCollapseToggleClick = (idx) => {
+        const newExpandedByIdx = {...this.state.expandedByIdx}
+
+        if (this.isExpanded(idx)) {
+            delete newExpandedByIdx[idx]
+        } else {
+            newExpandedByIdx[idx] = true
+        }
+
+        this.setState({expandedByIdx: newExpandedByIdx})
     }
 }
 
