9f86cfd442627f8e26c94dcf76de363b981f8920
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-05-28 11:35:45 -0400

    add full boxes around nested api params

--- td-documentation-reactjs/src/doc-elements/api/ApiParameters.js
+++ td-documentation-reactjs/src/doc-elements/api/ApiParameters.js
@@ -4,12 +4,10 @@ import ApiParameter from './ApiParameter'
 import './ApiParameters.css'
 
 export default function ApiParameters({parameters, nestedLevel, parentWidth = 0, elementsLibrary}) {
-    const isExpanded = parameters.length === 1 && parameters[0].children
-
     const renderedParameters = parameters.map(p => <ApiParameter key={p.name}
                                                                  name={p.name}
                                                                  type={p.type}
-                                                                 isExpanded={isExpanded}
+                                                                 isExpanded={false}
                                                                  children={p.children}
                                                                  description={p.description}
                                                                  nestedLevel={nestedLevel}
