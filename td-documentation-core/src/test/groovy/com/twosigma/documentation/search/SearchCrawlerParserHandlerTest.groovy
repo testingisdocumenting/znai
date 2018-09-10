2f6cf3a3e698b4e38e52c9bdaf08e4561839a5ac
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-09-10 07:01:36 -0400

    stop breaking multi-style word in separate pieces for search data

--- td-documentation-core/src/test/groovy/com/twosigma/documentation/search/SearchCrawlerParserHandlerTest.groovy
+++ td-documentation-core/src/test/groovy/com/twosigma/documentation/search/SearchCrawlerParserHandlerTest.groovy
@@ -19,19 +19,34 @@ class SearchCrawlerParserHandlerTest {
     void "should create search entry per section"() {
         parserHandler.onSectionStart('section one')
         parserHandler.onSimpleText('hello')
-        parserHandler.onSnippet(PluginParams.EMPTY, 'na', '', 'source code')
+        parserHandler.onSnippet(PluginParams.EMPTY, '', '', 'source code')
         parserHandler.onInlinedCode('inlined term')
         parserHandler.onSectionEnd()
 
         parserHandler.onSectionStart('section two')
         parserHandler.onSimpleText('world')
-        parserHandler.onSnippet(PluginParams.EMPTY, 'na', '', 'code')
+        parserHandler.onSnippet(PluginParams.EMPTY, '', '', 'code')
         parserHandler.onInlinedCode('broker')
         parserHandler.onSectionEnd()
 
-        parserHandler.getSearchEntries().should == [ 'pageSectionTitle' | 'text'] {
+        parserHandler.getSearchEntries().should == [ 'pageSectionTitle' | 'searchText'] {
                                                      _______________________________
                                                      'section one'      | [text: 'hello source code inlined term', score: SearchScore.STANDARD]
                                                      'section two'      | [text: 'world code broker', score: SearchScore.STANDARD] }
+    }
+
+    @Test
+    void "should connect mixed styles characters within a word without extra spaces"() {
+        parserHandler.onSectionStart('section')
+        parserHandler.onSimpleText('H')
+        parserHandler.onEmphasisStart()
+        parserHandler.onSimpleText('el')
+        parserHandler.onEmphasisEnd()
+        parserHandler.onStrongEmphasisStart()
+        parserHandler.onSimpleText('lo')
+        parserHandler.onStrongEmphasisEnd()
+        parserHandler.onSectionEnd()
+
+        parserHandler.getSearchEntries().searchText.text.should == ['Hello']
     }
 }
