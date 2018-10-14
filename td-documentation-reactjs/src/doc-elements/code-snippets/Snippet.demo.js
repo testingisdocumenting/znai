0f8b57db1e7c70abb68ce32c149eb677232aeb0a
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-10-13 20:25:36 -0400

    remove extra margins from code snippets and paragraph in two sides mode

--- td-documentation-reactjs/src/doc-elements/code-snippets/Snippet.demo.js
+++ td-documentation-reactjs/src/doc-elements/code-snippets/Snippet.demo.js
@@ -7,27 +7,54 @@ import {elementsLibrary} from '../DefaultElementsLibrary'
 
 import {TwoSidesLayoutRightPart} from '../page/two-sides/TwoSidesLayout'
 
+import Paragraph from '../default-elements/Paragraph'
+import {Page} from '../page/Page'
+
 import './tokens.css'
 
 export function snippetsDemo(registry) {
     registry
         .add('title', <Snippet title="snippet title" lang="html" snippet={htmlCode()}/>)
         .add('wide with title', <Snippet wide={true} title="snippet title" lang="java" snippet={wideCode()}/>)
-        .add('with bullet points', <Snippet wide={false} lang="java" snippet={codeWithComments()} commentsType="inline"/>)
-        .add('with spoiler bullet points', <Snippet wide={false} lang="java" snippet={codeWithComments()}  spoiler={true} commentsType="inline"/>)
+        .add('with bullet points', <Snippet wide={false} lang="java" snippet={codeWithComments()}
+                                            commentsType="inline"/>)
+        .add('with spoiler bullet points', <Snippet wide={false} lang="java" snippet={codeWithComments()} spoiler={true}
+                                                    commentsType="inline"/>)
         .add('wide with bullet points', <Snippet wide={true} lang="java" snippet={wideCode()} commentsType="inline"/>)
-        .add('wide with bullet points right side background', <TwoSidesLayoutRightPart><Snippet wide={true} lang="java" snippet={wideCode()} commentsType="inline"/></TwoSidesLayoutRightPart>)
-        .add('wide with spoiler bullet points', <Snippet wide={true} spoiler={true} lang="java" snippet={wideCode()} commentsType="inline"/>)
+        .add('wide with bullet points right side background', <TwoSidesLayoutRightPart><Snippet wide={true} lang="java"
+                                                                                                snippet={wideCode()}
+                                                                                                commentsType="inline"/></TwoSidesLayoutRightPart>)
+        .add('wide with spoiler bullet points', <Snippet wide={true} spoiler={true} lang="java" snippet={wideCode()}
+                                                         commentsType="inline"/>)
         .add('horizontal scroll', <Snippet wide={false} lang="java" snippet={wideCode()}/>)
         .add('highlight by line idx', <Snippet lang="markdown" snippet={markdownCode()} highlight={[0]}/>)
         .add('highlight by text', <Snippet lang="markdown" snippet={markdownCode()} highlight={"include-file"}/>)
-        .add('highlight by text right side background', <TwoSidesLayoutRightPart><Snippet lang="java" snippet={wideCode()} highlight={"createMegaAbstractFactory"}/></TwoSidesLayoutRightPart>)
+        .add('highlight by text right side background', <TwoSidesLayoutRightPart><Snippet lang="java"
+                                                                                          snippet={wideCode()}
+                                                                                          highlight={"createMegaAbstractFactory"}/></TwoSidesLayoutRightPart>)
         .add('read more', <Snippet lang="csv" snippet={longCode()}
                                    readMore={true} readMoreVisibleLines={4}/>)
         .add('tabs with wide', <Tabs {...tabsContent({label: 'wide', wide: true})}
                                      elementsLibrary={elementsLibrary}/>)
         .add('tabs with narrow', <Tabs {...tabsContent({label: 'narrow', wide: false})}
                                        elementsLibrary={elementsLibrary}/>)
+}
+
+export function snippetsTwoSidesDemo(registry) {
+    registry
+        .add('code after heading', <Page elementsLibrary={elementsLibrary}
+                                         docMeta={docMeta()}
+                                         {...twoSidesPage(snippetAfterSection())}/>)
+        .add('text between code blocks', <Page elementsLibrary={elementsLibrary}
+                                               docMeta={docMeta()}
+                                               {...twoSidesPage(textBetweenSnippetsMultipleBlocks())}/>)
+}
+
+function docMeta() {
+    return {
+        "title": "Doc Title",
+        "type": "Guide"
+    }
 }
 
 function htmlCode() {
@@ -71,11 +98,88 @@ function longCode() {
 }
 
 function tabsContent({label, wide}) {
-    return {tabsContent: [{
+    return {
+        tabsContent: [{
             name: label, content: [{
                 type: "Snippet",
                 wide: wide,
                 lang: "java",
                 snippet: wideCode()
-            }]}]}
+            }]
+        }]
+    }
+}
+
+function twoSidesPage(content) {
+    return {
+        "type": "Page",
+        "content": content,
+        "lastModifiedTime": 1527473295000,
+        "tocItem": {
+            "sectionTitle": "Demo",
+            "pageTitle": "Demo",
+            "pageMeta": {
+                "type": [
+                    "two-sides"
+                ]
+            },
+            "fileName": "demo",
+            "dirName": "demo",
+            "pageSectionIdTitles": []
+        }
+    }
+}
+
+function snippetAfterSection() {
+    return [
+        {
+            "title": "Primary Use Case",
+            "id": "primary-use-case",
+            "type": "Section",
+            "content": [contentSnippet(false)]
+        }
+    ]
+}
+
+function textBetweenSnippetsMultipleBlocks() {
+    return [
+        {
+            "title": "Primary Use Case",
+            "id": "primary-use-case",
+            "type": "Section",
+            "content": [
+                contentSnippet(false),
+                contentSnippet(true),
+                contentParagraph(false),
+                contentParagraph(true),
+                contentSnippet(false),
+            ]
+        }
+    ]
+}
+
+function contentParagraph(isRightSide) {
+    return {
+        "type": "Paragraph",
+        "content": [
+            {
+                "text": "The sea had jeeringly kept his finite body up, but drowned the infinite of his soul. Not drowned entirely, though. Rather carried down alive to wondrous depths, where strange shapes of the unwarped primal world glided to and fro before his passive eyes; and the miser-merman, Wisdom, revealed his hoarded heaps; and among the joyous, heartless, ever-juvenile eternities, Pip saw the multitudinous, God-omnipresent, coral insects, that out of the firmament of waters heaved the colossal orbs.",
+                "type": "SimpleText"
+            }
+        ],
+        "meta": {
+            "rightSide": isRightSide
+        }
+    }
+}
+
+function contentSnippet(isRightSide) {
+    return {
+        "type": "Snippet",
+        "lang": "java",
+        "snippet": codeWithComments(),
+        "meta": {
+            "rightSide": isRightSide
+        }
+    }
 }
