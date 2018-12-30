187829d1eed3cc5758205d213be95103c4652516
MykolaGolubyev <golubev.nikolay@gmail.com>
2018-12-29 19:36:05 -0500

    align snippets explanation bullets with code

--- td-documentation-reactjs/src/doc-elements/code-snippets/CircleBadge.js
+++ td-documentation-reactjs/src/doc-elements/code-snippets/CircleBadge.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import './CircleBadge.css'
+
 const CircleBadge = ({idx}) => {
-    return <span className="ui blue circular label circle-badge">{idx}</span>
+    return <span className="circle-badge">{idx}</span>
 }
 
 export default CircleBadge
