fbe2fa996e5b1916c543a40a9d05e1f656741d7a
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-06-27 22:47:52 -0400

    make search trigger always visible in case of long TOC

--- td-documentation-reactjs/src/doc-elements/structure/toc/TocPanelSearch.js
+++ td-documentation-reactjs/src/doc-elements/structure/toc/TocPanelSearch.js
@@ -5,10 +5,12 @@ import './TocPanelSearch.css'
 class TocPanelSearch extends Component {
     render() {
         return (
-            <div className="mdoc-toc-panel-search" onClick={this.props.onClick} title="hotkey /">
-                <div className="mdoc-toc-panel-search-icon-and-text">
-                    <div className="mdoc-search-icon glyphicon glyphicon-search"/>
-                    <div>Search...</div>
+            <div className="mdoc-toc-panel-search-area">
+                <div className="mdoc-toc-panel-search" onClick={this.props.onClick} title="hotkey /">
+                    <div className="mdoc-toc-panel-search-icon-and-text">
+                        <div className="mdoc-search-icon glyphicon glyphicon-search"/>
+                        <div>Search...</div>
+                    </div>
                 </div>
             </div>
         )
