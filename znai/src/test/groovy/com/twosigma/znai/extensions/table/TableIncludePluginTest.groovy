b8652eeac44c5a92b4cc7ca48a35ac992a171b1f
Mykola <golubev.nikolay@gmail.com>
2019-12-15 23:44:06 -0500

    investigate CI failed test

--- znai/src/test/groovy/com/twosigma/znai/extensions/table/TableIncludePluginTest.groovy
+++ znai/src/test/groovy/com/twosigma/znai/extensions/table/TableIncludePluginTest.groovy
@@ -24,44 +24,44 @@ class TableIncludePluginTest {
     void "should read table from csv by detecting format"() {
         def element = process('test-table.csv')
 
-        assert element == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Price'], [title: 'Description']],
-                                                  data:[[[[type:'TestMarkup', markup: '#12BGD3']], [[type:'TestMarkup', markup: '100']],
-                                                         [[type:'TestMarkup', markup: 'custom table with a long attachment']]],
-                                                        [[[type:'TestMarkup', markup: '#12BGD3']], [[type:'TestMarkup', markup: '150']],
-                                                         [[type:'TestMarkup', markup: 'chair']]]]]]
+        element.shoul == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Price'], [title: 'Description']],
+                                                 data   : [[[[type: 'TestMarkup', markup: '#12BGD3']], [[type: 'TestMarkup', markup: '100']],
+                                                            [[type: 'TestMarkup', markup: 'custom table with a long attachment']]],
+                                                           [[[type: 'TestMarkup', markup: '#12BGD3']], [[type: 'TestMarkup', markup: '150']],
+                                                            [[type: 'TestMarkup', markup: 'chair']]]]]]
     }
 
     @Test
     void "should read table from json by detecting format"() {
         def element = process('test-table.json')
 
-        assert element == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Price'], [title: 'Description']],
-                                                  data:[[[[type:'TestMarkup', markup: '#12BGD3']], [[type:'TestMarkup', markup: '100']],
-                                                         [[type:'TestMarkup', markup: 'custom table with a long attachment']]],
-                                                        [[[type:'TestMarkup', markup: '#12BGD3']], [[type:'TestMarkup', markup: '150']],
-                                                         [[type:'TestMarkup', markup: 'chair']]]]]]
+        element.should == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Price'], [title: 'Description']],
+                                                  data   : [[[[type: 'TestMarkup', markup: '#12BGD3']], [[type: 'TestMarkup', markup: '100']],
+                                                             [[type: 'TestMarkup', markup: 'custom table with a long attachment']]],
+                                                            [[[type: 'TestMarkup', markup: '#12BGD3']], [[type: 'TestMarkup', markup: '150']],
+                                                             [[type: 'TestMarkup', markup: 'chair']]]]]]
     }
 
     @Test
     void "should filter out columns from json based on case insensitive provided names"() {
         def element = process('test-table.json', '{columns: ["account", "description"]}')
 
-        assert element == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Description']],
-                                                  data:[[[[type:'TestMarkup', markup: '#12BGD3']],
-                                                         [[type:'TestMarkup', markup: 'custom table with a long attachment']]],
-                                                        [[[type:'TestMarkup', markup: '#12BGD3']],
-                                                         [[type:'TestMarkup', markup: 'chair']]]]]]
+        element.should == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Description']],
+                                                  data   : [[[[type: 'TestMarkup', markup: '#12BGD3']],
+                                                             [[type: 'TestMarkup', markup: 'custom table with a long attachment']]],
+                                                            [[[type: 'TestMarkup', markup: '#12BGD3']],
+                                                             [[type: 'TestMarkup', markup: 'chair']]]]]]
     }
 
     @Test
     void "should filter out columns from csv based on case insensitive provided names"() {
         def element = process('test-table.csv', '{columns: ["account", "description"]}')
 
-        assert element == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Description']],
-                                                  data:[[[[type:'TestMarkup', markup: '#12BGD3']],
-                                                         [[type:'TestMarkup', markup: 'custom table with a long attachment']]],
-                                                        [[[type:'TestMarkup', markup: '#12BGD3']],
-                                                         [[type:'TestMarkup', markup: 'chair']]]]]]
+        element.should == [type: 'Table', table: [columns: [[title: 'Account'], [title: 'Description']],
+                                                  data   : [[[[type: 'TestMarkup', markup: '#12BGD3']],
+                                                             [[type: 'TestMarkup', markup: 'custom table with a long attachment']]],
+                                                            [[[type: 'TestMarkup', markup: '#12BGD3']],
+                                                             [[type: 'TestMarkup', markup: 'chair']]]]]]
     }
 
     private static def process(String fileName, String meta = '') {
