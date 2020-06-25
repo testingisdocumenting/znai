fae574da19cb120168151fcab1e75d103f70fdd8
Mykola <golubev.nikolay@gmail.com>
2020-06-25 17:35:00 -0400

    display icons in presentation horizontal stripes

--- znai-reactjs/src/doc-elements/bullets/bulletUtils.test.js
+++ znai-reactjs/src/doc-elements/bullets/bulletUtils.test.js
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2020 znai maintainers
  * Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,124 +20,33 @@ import {
     extractIconId,
     removeIcon,
     extractTextLinesEmphasisOnly,
-    extractTextLinesEmphasisOrFull
+    extractTextLinesEmphasisOrFull, extractIconIds
 } from './bulletUtils'
 
-const itemContent = [
-    {
-        "type": "Paragraph",
-        "content": [
-            {
-                "type": "Icon",
-                "id": "time"
-            },
-            {
-                "type": "Emphasis",
-                "content": [
-                    {
-                        "text": "Inject",
-                        "type": "SimpleText"
-                    }
-                ]
-            },
-            {
-                "text": " ",
-                "type": "SimpleText"
-            },
-            {
-                "code": "DAO",
-                "type": "InlinedCode"
-            },
-            {
-                "text": " to decouple",
-                "type": "SimpleText"
-            }
-        ]
-    }
-]
-
-const lowerCasedItemContent = [
-    {
-        "type": "Paragraph",
-        "content": [
-            {
-                "type": "Icon",
-                "id": "time"
-            },
-            {
-                "type": "Emphasis",
-                "content": [
-                    {
-                        "text": "inject",
-                        "type": "SimpleText"
-                    }
-                ]
-            }
-        ]
-    }
-]
-
-
-const bulletListContent = [
-    {
-        "type": "ListItem",
-        "content": [
-            {
-                "type": "Paragraph",
-                "content": [
-                    {
-                        "text": "Additional",
-                        "type": "SimpleText"
-                    },
-                    {
-                        "type": "Emphasis",
-                        "content": [
-                            {
-                                "text": "Inject",
-                                "type": "SimpleText"
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "type": "ListItem",
-        "content": [
-            {
-                "type": "Paragraph",
-                "content": [
-                    {
-                        "text": "Third Step with text",
-                        "type": "SimpleText"
-                    }
-                ]
-            }
-        ]
-    }
-]
+const itemContentWithIcon = buildItemContentWithIcon()
+const lowerCasedItemContent = buildLowerCasedItemContent()
+const bulletListContent = buildBulletListContent()
 
 describe("bulletUtils", () => {
     it("detects if bullet item starts with Icon", () => {
-        expect(startsWithIcon(itemContent)).toBeTruthy()
+        expect(startsWithIcon(itemContentWithIcon)).toBeTruthy()
     })
 
     it("extracts Icon id", () => {
-        expect(extractIconId(itemContent)).toEqual("time")
+        expect(extractIconId(itemContentWithIcon)).toEqual("time")
     })
 
     it("removes icon from content", () => {
-        const originalSize = itemContent[0].content.length
+        const originalSize = itemContentWithIcon[0].content.length
 
-        const withoutIcon = removeIcon(itemContent)
+        const withoutIcon = removeIcon(itemContentWithIcon)
 
         expect(withoutIcon[0].content[0].type).toEqual("Emphasis")
-        expect(itemContent[0].content.length).toEqual(originalSize)
+        expect(itemContentWithIcon[0].content.length).toEqual(originalSize)
     })
 
     it("extracts only emphasised text", () => {
-        const extractedTexts = extractTextLinesEmphasisOnly(itemContent)
+        const extractedTexts = extractTextLinesEmphasisOnly(itemContentWithIcon)
         expect(extractedTexts).toEqual(["Inject"])
     })
 
@@ -149,4 +59,150 @@ describe("bulletUtils", () => {
         const extractedTexts = extractTextLinesEmphasisOnly(lowerCasedItemContent)
         expect(extractedTexts).toEqual(["Inject"])
     })
+
+    it("extracts icon ids", () => {
+        const iconIds = extractIconIds([
+            listItemContent(itemContentWithIcon),
+            listItemContent(buildItemContentWithoutIcon()),
+            listItemContent(itemContentWithIcon)])
+        expect(iconIds).toEqual(["time", undefined, "time"])
+    })
 })
+
+function listItemContent(content) {
+    return {
+        type: "ListItem",
+        content
+    }
+}
+
+function buildItemContentWithIcon() {
+    return [
+        {
+            "type": "Paragraph",
+            "content": [
+                {
+                    "type": "Icon",
+                    "id": "time"
+                },
+                {
+                    "type": "Emphasis",
+                    "content": [
+                        {
+                            "text": "Inject",
+                            "type": "SimpleText"
+                        }
+                    ]
+                },
+                {
+                    "text": " ",
+                    "type": "SimpleText"
+                },
+                {
+                    "code": "DAO",
+                    "type": "InlinedCode"
+                },
+                {
+                    "text": " to decouple",
+                    "type": "SimpleText"
+                }
+            ]
+        }
+    ];
+}
+
+function buildItemContentWithoutIcon() {
+    return [
+        {
+            "type": "Paragraph",
+            "content": [
+                {
+                    "type": "Emphasis",
+                    "content": [
+                        {
+                            "text": "Inject",
+                            "type": "SimpleText"
+                        }
+                    ]
+                },
+                {
+                    "text": " ",
+                    "type": "SimpleText"
+                },
+                {
+                    "code": "DAO",
+                    "type": "InlinedCode"
+                },
+                {
+                    "text": " to decouple",
+                    "type": "SimpleText"
+                }
+            ]
+        }
+    ];
+}
+
+function buildLowerCasedItemContent() {
+    return [
+        {
+            "type": "Paragraph",
+            "content": [
+                {
+                    "type": "Icon",
+                    "id": "time"
+                },
+                {
+                    "type": "Emphasis",
+                    "content": [
+                        {
+                            "text": "inject",
+                            "type": "SimpleText"
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+}
+
+function buildBulletListContent() {
+    return [
+        {
+            "type": "ListItem",
+            "content": [
+                {
+                    "type": "Paragraph",
+                    "content": [
+                        {
+                            "text": "Additional",
+                            "type": "SimpleText"
+                        },
+                        {
+                            "type": "Emphasis",
+                            "content": [
+                                {
+                                    "text": "Inject",
+                                    "type": "SimpleText"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "ListItem",
+            "content": [
+                {
+                    "type": "Paragraph",
+                    "content": [
+                        {
+                            "text": "Third Step with text",
+                            "type": "SimpleText"
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+}
