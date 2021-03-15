dbfda4cb941f3d581f5450baf3ea8940cee9e829
Mykola <golubev.nikolay@gmail.com>
2021-03-14 23:57:09 -0400

    visuals: handle badge annotation align

--- znai-reactjs/src/doc-elements/images/shapes/Circle.js
+++ znai-reactjs/src/doc-elements/images/shapes/Circle.js
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2021 znai maintainers
  * Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,15 +18,44 @@
 import React from 'react'
 import colorByName from './colorByName'
 
-const CircleBody = ({x, y, r, color, text, ...props}) => {
+const CircleBody = ({x, y, r, color, text, align, ...props}) => {
     const colorScheme = colorByName(color)
+
+    const [cx, cy] = calcCenter()
 
     return (
         <g>
-           <circle cx={x} cy={y} r={r} stroke={colorScheme.line} strokeWidth="4" fill={colorScheme.fill} strokeOpacity={1} fillOpacity={1} {...props}/>
-           <text x={x} y={y} fill={colorScheme.text} textAnchor="middle" alignmentBaseline="central">{text}</text>
+           <circle cx={cx} cy={cy} r={r} stroke={colorScheme.line} strokeWidth="4" fill={colorScheme.fill} strokeOpacity={1} fillOpacity={1} {...props}/>
+           <text x={cx} y={cy} fill={colorScheme.text} textAnchor="middle" alignmentBaseline="central">{text}</text>
         </g>
     );
+
+    function calcCenter() {
+        const d = r * 2
+        return [x + deltaX(), y + deltaY()]
+
+        function deltaX() {
+            switch (align) {
+                case 'ToTheLeft':
+                    return -d
+                case 'ToTheRight':
+                    return d
+                default:
+                    return 0
+            }
+        }
+
+        function deltaY() {
+            switch (align) {
+                case 'Above':
+                    return -d
+                case 'Below':
+                    return d
+                default:
+                    return 0
+            }
+        }
+    }
 }
 
 const circle = {
