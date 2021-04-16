d49c1c82a93d60d99c52e960dd454216b56d732b
Mykola <golubev.nikolay@gmail.com>
2021-04-15 20:40:10 -0400

    snippets: no wrap for inlined code + tsx conversion

---
+++ znai-reactjs/src/doc-elements/code-snippets/InlinedCode.demo.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 znai maintainers
+ * Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+
+import { InlinedCode } from "./InlinedCode";
+import Link from "../default-elements/Link";
+import { elementsLibrary } from "../DefaultElementsLibrary";
+import { Registry } from "react-component-viewer";
+
+export function inlinedCodeDemo(registry: Registry) {
+  registry
+    .add("regular", () => <InlinedCode code="ClassName" />)
+    .add("no break", () => (
+      <div style={{ width: 100 }}>
+        <InlinedCode code="my class name of something" />
+      </div>
+    ))
+    .add("with global doc ref", () => <InlinedCode code="package.SuperClass" />)
+    .add("with local doc ref", () => (
+      <InlinedCode
+        code="another.SuperClass"
+        references={{
+          "another.SuperClass": {
+            pageUrl: "#another-url",
+          },
+        }}
+      />
+    ))
+    .add("inside link tet", () => (
+      <Link
+        url="#url"
+        isFile={false}
+        content={linkContent()}
+        elementsLibrary={elementsLibrary}
+      />
+    ));
+}
+
+function linkContent() {
+  return [
+    {
+      type: "SimpleText",
+      text: "text ",
+    },
+    {
+      type: "InlinedCode",
+      code: "superCode",
+    },
+    {
+      type: "SimpleText",
+      text: " text ",
+    },
+  ];
+}
---
+++ znai-reactjs/src/doc-elements/references/ReferenceLinkWrapper.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 znai maintainers
+ * Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+import { isExternalUrl } from "../../structure/links";
+import { documentationNavigation } from "../../structure/DocumentationNavigation";
+import { LinkWrapper } from "../default-elements/LinkWrapper";
+
+interface Props {
+  referenceUrl: string;
+  className?: string;
+  children: any;
+}
+
+export function ReferenceLinkWrapper({
+  referenceUrl,
+  className,
+  children,
+}: Props) {
+  if (!referenceUrl) {
+    return children;
+  }
+
+  const isLocalNavigation = !isExternalUrl(referenceUrl);
+
+  const fullUrl = isLocalNavigation
+    ? documentationNavigation.fullPageUrl(referenceUrl)
+    : referenceUrl;
+
+  return (
+    <LinkWrapper
+      url={fullUrl}
+      treatAsLocal={isLocalNavigation}
+      className={className}
+    >
+      {children}
+    </LinkWrapper>
+  );
+}
