57c94a71ccf5f1e0ebb540d662ca8ea5bf75f0ab
Karol Dudzinski <karol.dudzinski@twosigmaiq.com>
2021-07-12 11:45:51 +0100

    python: support extraction of assignments

--- znai-python/src/test/groovy/org/testingisdocumenting/znai/python/PythonDocIncludePluginTest.groovy
+++ znai-python/src/test/groovy/org/testingisdocumenting/znai/python/PythonDocIncludePluginTest.groovy
@@ -48,7 +48,8 @@ class PythonDocIncludePluginTest {
         code {
             resultingProps('example.py', '{entry: "my_func_two"}')
         } should throwException("can't find entry: my_func_two in: example.py, available entries: " +
-                "a_method, func_no_docs, my_func, another_func, AClass, AClass.foo, Animal, Animal.says")
+                "a_method, func_no_docs, my_func, another_func, AClass, AClass.foo, Animal, Animal.says, " +
+                "one_line_var, multi_line_var")
     }
 
     private static Map<String, Object> resultingProps(String fileName, String value) {
