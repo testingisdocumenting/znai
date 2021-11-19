4b0304cd064feec332b7d929041af4c593912c17
Mykola Golubyev <golubev.nikolay@gmail.com>
2021-11-19 12:59:40 -0500

    doxygen: fix full member by args (#712)

--- znai-docs/znai/doxygen/src/funcs_two.h
+++ znai-docs/znai/doxygen/src/funcs_two.h
@@ -4,6 +4,12 @@ namespace nested {
 void their_func(long param1, std::string param2, bool param3) {
 }
 
+/**
+ * Description of *what* and *why* for their_func
+ *
+ * @param param1 description of param1
+ * @param param3 description of param3
+ */
 void their_func(long param1, bool param3) {
 }
 
