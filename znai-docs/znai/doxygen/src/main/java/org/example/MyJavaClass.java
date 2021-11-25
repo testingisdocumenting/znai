5aba0ac5aacab13b2254af429d2e555a059f5b82
mykolagolubyev <golubev.nikolay@gmail.com>
2021-11-25 13:26:45 -0500

    java in progress

---
+++ znai-docs/znai/doxygen/src/main/java/org/example/MyJavaClass.java
@@ -0,0 +1,17 @@
+package java.org.example;
+
+/**
+ * my java class
+ */
+class MyJavaClass {
+    /**
+     * hello world
+     */
+    public void hello() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
