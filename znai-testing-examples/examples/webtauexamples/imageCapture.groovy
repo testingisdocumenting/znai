ec9e58758285c46703216ec8110d9e70c174aa8b
Mykola Golubyev <golubev.nikolay@gmail.com>
2022-08-17 12:35:20 -0400

    doc: webtau browser example fix (#950)

--- znai-testing-examples/examples/webtauexamples/imageCapture.groovy
+++ znai-testing-examples/examples/webtauexamples/imageCapture.groovy
@@ -2,20 +2,18 @@ package webtauexamples
 
 import static org.testingisdocumenting.webtau.WebTauGroovyDsl.*
 
-def homeSearchInput = $("#search_form_input_homepage")
+def homeSearchInput = $('input[id*="search"]')
 def resultSearchInput = $("#search_form_input")
-def searchButton = $("#search_button_homepage")
 def result = $('article[data-testid="result"]')
 
 scenario("capture screenshot") {
     browser.open("https://duckduckgo.com/")
 
-    homeSearchInput.setValue("testing is documenting")
-    searchButton.click()
+    homeSearchInput.waitToBe visible
+    homeSearchInput.setValue("testing is documenting\n")
 
     result.waitTo visible
 
-    browser.doc.withAnnotations(
-            browser.doc.badge(resultSearchInput),
-            browser.doc.badge(result)).capture("duckduckgo-search")
+    browser.doc.withAnnotations(resultSearchInput, result)
+            .capture("duckduckgo-search")
 }
