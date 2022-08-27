1550a8ba1418333c4b198bd7509f24a82f992039
Mykola Golubyev <golubev.nikolay@gmail.com>
2022-08-27 19:47:10 -0400

    react: initial base components for titles (#976)

---
+++ znai-reactjs/src/doc-elements/title/ContainerTitle.demo.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 znai maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+import { Registry, simulateState } from "react-component-viewer";
+import { ContainerTitle } from "./ContainerTitle";
+
+import "./ContainerTitle.demo.css";
+
+const [getCollapsed, setCollapsed] = simulateState(true);
+
+export function containerTitleDemo(registry: Registry) {
+  registry
+    .add("only title", () => <ContainerTitle title="snippet title" />)
+    .add("with collapse", () => (
+      <ContainerTitle title="snippet title" collapsed={getCollapsed()} onCollapseToggle={toggleCollapse} />
+    ))
+    .add("with anchor", () => <ContainerTitle title="snippet title" anchorId="my-code-anchor" />)
+    .add("with collapse and anchor", () => (
+      <ContainerTitle
+        title="snippet title"
+        collapsed={getCollapsed()}
+        onCollapseToggle={toggleCollapse}
+        anchorId="my-code-anchor"
+      />
+    ))
+    .add("custom class", () => (
+      <ContainerTitle
+        title="snippet title"
+        additionalTitleClassNames="demo-center"
+        collapsed={getCollapsed()}
+        onCollapseToggle={toggleCollapse}
+        anchorId="my-code-anchor"
+      />
+    ));
+}
+
+function toggleCollapse() {
+  setCollapsed(!getCollapsed());
+}
---
+++ znai-reactjs/src/doc-elements/title/ContainerTitle.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 znai maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+import { Icon } from "../icons/Icon";
+
+import "./ContainerTitle.css";
+
+interface Props {
+  title: string;
+  additionalContainerClassNames?: string;
+  additionalTitleClassNames?: string;
+  collapsed?: boolean;
+  anchorId?: string;
+
+  onCollapseToggle?(): void;
+}
+
+/**
+ * title strip for snippets, api parameters, tables, images, etc
+ * with collapse option, noGap and anchor
+ */
+export function ContainerTitle({
+  title,
+  additionalContainerClassNames,
+  additionalTitleClassNames,
+  collapsed,
+  anchorId,
+  onCollapseToggle,
+}: Props) {
+  const collapsible = collapsed !== undefined;
+
+  const containerClassName =
+    "znai-container-title-wrapper" +
+    (collapsible ? " collapsible" : "") +
+    (additionalContainerClassNames ? " " + additionalContainerClassNames : "");
+
+  const titleClassName =
+    "znai-container-title" +
+    (collapsible ? " collapsible" : "") +
+    (additionalTitleClassNames ? " " + additionalTitleClassNames : "");
+
+  return (
+    <div className={containerClassName} id={anchorId}>
+      {collapsible && (
+        <div className="znai-container-title-collapse-toggle" onClick={onCollapseToggle}>
+          {collapsed ? "+" : "-"}
+        </div>
+      )}
+
+      <div className={titleClassName}>{title}</div>
+
+      {anchorId && (
+        <div className="znai-container-title-anchor">
+          <a href={"#" + anchorId}>
+            <Icon id="link" />
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
