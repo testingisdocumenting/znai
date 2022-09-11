412b6c3392d46ee563aa68c72a64b55b58aace17
mykolagolubyev <golubev.nikolay@gmail.com>
2022-09-11 19:32:14 -0400

    cards: link support

--- znai-reactjs/src/doc-elements/card/Card.tsx
+++ znai-reactjs/src/doc-elements/card/Card.tsx
@@ -18,6 +18,10 @@ import React from "react";
 
 import { DocElementContent, WithElementsLibrary } from "../default-elements/DocElement";
 
+import { extractLinkTextAndUrl, isContentElementSelfContainedLink } from "./cardContentAnalyzer";
+import { LinkWrapper } from "../default-elements/LinkWrapper";
+import { isLocalUrl } from "../../structure/links";
+
 import "./Card.css";
 
 interface Props extends WithElementsLibrary {
@@ -29,7 +33,12 @@ interface Props extends WithElementsLibrary {
 }
 
 export function Card({ title, elementsLibrary, imageSrc, imageHeight, imageBackground, bodyContent }: Props) {
-  const cardClassName = "znai-card " + (isLastElementContentContainer() ? " no-bottom-padding" : "");
+  const lastNonLinkIdx = findIndexOfLastNonLinkElement();
+
+  const links = extractLinks();
+
+  const cardClassName = "znai-card " + (isLastNonLinkElementContentContainer() ? " no-bottom-padding" : "");
+
   return (
     <div className="znai-card-wrapper content-block">
       <div className={cardClassName}>
@@ -41,19 +50,56 @@ export function Card({ title, elementsLibrary, imageSrc, imageHeight, imageBackg
         <div className="znai-card-content">
           {title && <div className="znai-card-title">{title}</div>}
           <div className="znai-card-body">
-            <elementsLibrary.DocElement content={bodyContent} elementsLibrary={elementsLibrary} />
+            <elementsLibrary.DocElement content={contentWithoutLinksAtTheEnd()} elementsLibrary={elementsLibrary} />
           </div>
         </div>
+        {links.length > 0 && <div className="znai-card-links">{renderLinks(links)}</div>}
       </div>
     </div>
   );
 
-  function isLastElementContentContainer() {
+  function renderLinks(links: Array<{ text: string; url: string }>) {
+    return links.map((link, idx) => (
+      <div className="znai-card-link" key={idx}>
+        <LinkWrapper url={link.url} treatAsLocal={isLocalUrl(link.url)}>
+          {link.text}
+        </LinkWrapper>
+      </div>
+    ));
+  }
+
+  function contentWithoutLinksAtTheEnd() {
+    return bodyContent.slice(0, lastNonLinkIdx + 1);
+  }
+
+  function extractLinks() {
+    const result = [];
+    for (let idx = lastNonLinkIdx + 1; idx < bodyContent.length; idx++) {
+      result.push(extractLinkTextAndUrl(bodyContent[idx]));
+    }
+
+    return result;
+  }
+
+  function findIndexOfLastNonLinkElement() {
+    let lastNonLinkIdx = bodyContent.length - 1;
+    while (lastNonLinkIdx > 0) {
+      if (isContentElementSelfContainedLink(bodyContent[lastNonLinkIdx])) {
+        lastNonLinkIdx--;
+      } else {
+        break;
+      }
+    }
+
+    return lastNonLinkIdx;
+  }
+
+  function isLastNonLinkElementContentContainer() {
     if (bodyContent.length === 0) {
       return false;
     }
 
-    const type = bodyContent[bodyContent.length - 1].type;
+    const type = bodyContent[lastNonLinkIdx].type;
     return type === "Snippet" || type === "ApiParameters";
   }
 }
