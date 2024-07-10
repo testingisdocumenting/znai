370c80552ba53cf4789ad56cb5f7d75fe7fabb6b
Mykola Golubyev <golubev.nikolay@gmail.com>
2024-07-09 22:13:15 -0400

    search: better code snippets processing (#1219)

--- znai-java/src/test/groovy/org/testingisdocumenting/znai/java/extensions/JavaDocIncludePluginTest.groovy
+++ znai-java/src/test/groovy/org/testingisdocumenting/znai/java/extensions/JavaDocIncludePluginTest.groovy
@@ -31,7 +31,7 @@ class JavaDocIncludePluginTest {
     @Test
     void "method in doc without signature"() {
         def plugin = processAndGetPlugin('{entry: "sampleMethod"}')
-        plugin.textForSearch().text.should == 'method level java doc package.Class'
+        plugin.textForSearch().text.should == 'method level java doc package Class'
     }
 
     @Test
@@ -46,13 +46,13 @@ class JavaDocIncludePluginTest {
         // the rest of search text is not present due to TestMarkdownParser ignores fine grained elements and therefore
         // SearchCrawler doesn't get enough info.
         // textForSearch is fully tested in HtmlToDocElementConverterTest
-        pluginAndParserHandler.includePlugin.textForSearch().text.should == 'package.Class'
+        pluginAndParserHandler.includePlugin.textForSearch().text.should == 'package Class'
     }
 
     @Test
     void "method in doc inner class"() {
         def plugin = processAndGetPlugin('{entry: "InnerClass.sampleMethod"}')
-        plugin.textForSearch().text.should == 'inner class method level java doc package.Class'
+        plugin.textForSearch().text.should == 'inner class method level java doc package Class'
     }
 
     @Test
@@ -76,7 +76,7 @@ class JavaDocIncludePluginTest {
     @Test
     void "variable with a link in doc"() {
         def plugin = processAndGetPlugin('{entry: "SAMPLE_CONST_WITH_LINK"}')
-        plugin.textForSearch().text.should == 'variable level java doc package.Class'
+        plugin.textForSearch().text.should == 'variable level java doc package Class'
     }
 
     private static IncludePlugin processAndGetPlugin(String params) {
