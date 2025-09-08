d8a526a64227b7322efecde19e8accda6cab286a
MykolaGolubyev <golubev.nikolay@gmail.com>
2025-09-08 19:53:39 -0400

    slack: remove trailing slash from `questionId`

--- znai-reactjs/src/doc-elements/text-selection/SlackActiveQuestions.tsx
+++ znai-reactjs/src/doc-elements/text-selection/SlackActiveQuestions.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 znai maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { useEffect, useState } from "react";
 import { currentPageId, pageIdFromTocItem } from "../../structure/DocumentationNavigation";
 import { getDocMeta } from "../../structure/docMeta";
@@ -7,6 +23,7 @@ import { TocItem } from "../../structure/TocItem";
 
 import { ResolveQuestionButton } from "./ResolveQuestionButton";
 import "./SlackActiveQuestions.css";
+import { removeTrailingSlashFromQueryParam } from "./queryParamUtils";
 
 interface Question {
   id: string;
@@ -113,6 +130,8 @@ export function SlackActiveQuestions({ containerNode, tocItem }: { containerNode
       </div>
     );
 
+    const cleanedUpQuestionId = removeTrailingSlashFromQueryParam(questionId);
+
     return (
       <HighlightedText
         key={question.slackMessageTs}
@@ -123,7 +142,7 @@ export function SlackActiveQuestions({ containerNode, tocItem }: { containerNode
         question={question.question}
         context={question.context}
         additionalView={additionalView}
-        displayBubbleAndScrollIntoView={!!questionId && question.id === questionId}
+        displayBubbleAndScrollIntoView={!!cleanedUpQuestionId && question.id === cleanedUpQuestionId}
       />
     );
   });
