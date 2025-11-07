4927b6cbd5cc3fa123efb09933022a2e227ddefd
antoine <antoine.levy-lambert@ny.email.gs.com>
2025-11-07 11:08:05 -0800

    changing configuration to make it more plain vanilla

--- znai-reactjs/vite.config.ts
+++ znai-reactjs/vite.config.ts
@@ -1,62 +1,21 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// @ts-ignore
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-        jsxRuntime: 'classic',
-        jsxImportSource: 'react'
-      }
-  )],
-  esbuild: {
-    jsx: 'automatic',
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-  },
+  plugins: [react()],
   build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
+    rolldownOptions: {
       output: {
-        manualChunks: undefined
+        advancedChunks: {
+          groups: [
+            {
+              name: 'mermaid',
+              test: /node_modules[\\/]mermaid/,
+              priority: 20,
+            }]
+        }
       }
-    },
-    sourcemap: true
-  },
-  server: {
-    headers: {
-      // Ensure proper MIME types
-      'Content-Type': 'application/javascript',
-      // Allow proper CORS
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    },
-    // Force proper MIME types
-    fs: {
-      strict: true
-    },
-    // Ensure proper module loading
-    middlewareMode: false
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom','mermaid'],
-    exclude: []
-  },
-  // Ensure proper asset handling
-  assetsInclude: ['**/*.js', '**/*.mjs'],
-  // Proper module resolution
-  resolve: {
-    alias: {
-      'react/jsx-runtime': 'react/jsx-runtime',
-      'react/jsx-dev-runtime': 'react/jsx-dev-runtime',
-      'mermaid': 'mermaid/dist/mermaid.esm.min.mjs'
-
-    },
-    extensions: ['.js', '.jsx', '.mjs', '.json', '.ts', '.tsx']
+    }
   }
-
-});
+})
