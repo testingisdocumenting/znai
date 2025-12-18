36687a3f1b265489f06ca43c5eb7d4c88780940e
antoine <antoine.levy-lambert@ny.email.gs.com>
2025-12-18 11:51:09 -0800

    updating copyrights

--- znai-reactjs/src/doc-elements/markdown/MarkdownAndResult.jsx
+++ znai-reactjs/src/doc-elements/markdown/MarkdownAndResult.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 TWO SIGMA OPEN SOURCE, LLC
+ * Copyright 2025 znai maintainers
  * Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
