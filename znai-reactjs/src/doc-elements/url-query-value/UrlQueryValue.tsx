3513ffcb8a95ec33400accfa2c9d604b1797e4ff
MykolaGolubyev <golubev.nikolay@gmail.com>
2026-04-14 21:05:19 -0400

    update copyright

--- znai-reactjs/src/doc-elements/url-query-value/UrlQueryValue.tsx
+++ znai-reactjs/src/doc-elements/url-query-value/UrlQueryValue.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 znai maintainers
+ * Copyright 2026 znai maintainers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
